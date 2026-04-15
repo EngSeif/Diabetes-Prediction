f2f3f4727f578e96b32ff986ff5f59c333ceae48
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-04-15 18:07:04 +0200

    chore(cleaning): moved cleaning logic into cleaning.py

---
+++ src/diabetes_prediction/cleaning/cleaning.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+
+class DataCleaning:
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    def remove_invalid_gender(self): 
+        self.df = self.df[~(self.df['gender'] == 'Other')]    
+        return self
+
+    def remove_inaccurate_infants(self):
+        self.df = self.df[~((self.df['age'] < 1) & (self.df['smoking_history'].isin(['not current', 'current'])))]
+        return self
+    
+    def remove_duplicates(self):
+        self.df = self.df.drop_duplicates(keep='first')
+        return self
+    
+    def run_cleaning(self):
+        return (
+            self
+            .remove_invalid_gender()
+            .remove_inaccurate_infants()
+            .remove_duplicates()
+            .df
+        )
