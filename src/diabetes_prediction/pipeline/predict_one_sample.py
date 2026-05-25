d77de37e853c0d8ff3798b9aaa70d29d3088a03e
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-05-25 10:53:11 +0300

    fix(deplyment): moved model import inside the function of prediction

--- src/diabetes_prediction/pipeline/predict_one_sample.py
+++ src/diabetes_prediction/pipeline/predict_one_sample.py
@@ -9,16 +9,7 @@ from src.diabetes_prediction.transformation.transformation import DataTransforma
 PROJECT_ROOT = Path(__file__).resolve().parents[3]
 sys.path.insert(0, str(PROJECT_ROOT))
 
-preprocessor_path = (
-    PROJECT_ROOT / "notebooks" / "Transformation" / "preprocessor.pkl"
-)
-model_path = PROJECT_ROOT / "models" / "RF_model.pkl"
 
-preprocessor = joblib.load(preprocessor_path)
-model = joblib.load(model_path)
-
-logging.info(preprocessor_path)
-logging.info(model_path)
 
 def predict_single_sample(user_input: dict) -> dict:
     """
@@ -30,6 +21,17 @@ def predict_single_sample(user_input: dict) -> dict:
     preprocessing and prediction behaviour.
     """
     try:
+
+        preprocessor_path = (
+            PROJECT_ROOT / "notebooks" / "Transformation" / "preprocessor.pkl"
+        )
+        model_path = PROJECT_ROOT / "models" / "RF_model.pkl"
+
+        preprocessor = joblib.load(preprocessor_path)
+        model = joblib.load(model_path)
+
+        logging.info(preprocessor_path)
+        logging.info(model_path)        
 
         transformer = DataTransformation()
         transformer.preprocessor = preprocessor  # use the saved preprocessor
